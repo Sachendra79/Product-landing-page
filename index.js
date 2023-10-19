c56ada428000258256f3fc8cd559f464345ed7d2
Sachendra79 <sachendragangwar120@gmail.com>
2023-10-19 17:24:25 +0530

    my final Product Landing Page

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-// function clearErrors(){
-//  errors = document.getElementsByClassName('formerror');
-//   for(let item of errors)
-//   {
-//       item.innerHTML = "";
-//   }
-// }
-// function seterror(id, error){
+function clearErrors() {
+    var errors = document.getElementsByClassName('formerror');
+    for (var i = 0; i < errors.length; i++) {
+        errors[i].innerHTML = "";
+    }
+  }
   
-//   element = document.getElementById(id);
-//   element.getElementsByClassName('formerror')[0].innerHTML = error;
-
-// }
-
-// function validateForm(){
-//   var returnval = true;
-//   clearErrors();
-//  var name = document.forms['myForm']["fname"].value;
-//   if (name.length<5){
-//       seterror("name", "*Length is too small:");
-//        returnval = false;
-//   }
-
-//   if (name.length == 0){
-//       seterror("name", "*Length Can't be Zero!");
-//        returnval = false;
-//   }
-
-//   var email = document.forms['myForm']["femail"].value;
-//   if (email.length>15){
-//       seterror("email", "*Use Short Email:");
-//        returnval = false;
-//   }
-
-//    return returnval;
-// }
-
-  document.getElementById("form").addEventListener("submit", function(event){
-    event.preventDefault() 
-    alert('Your message has been received');
-    });
-
-
-
+  function seterror(id, error) {
+    document.getElementById(id).nextElementSibling.innerHTML = error;
+  }
+  
+  function validateForm() {
+    var returnval = true;
+    clearErrors();
+    var name = document.forms['myForm']["fname"].value;
+    if (name.length < 5) {
+        seterror("name", "*Length is too small");
+        returnval = false;
+    }
+  
+    if (name.length === 0) {
+        seterror("name", "*Length Can't be Zero");
+        returnval = false;
+    }
+  
+    var email = document.forms['myForm']["femail"].value;
+    if (email.length > 30 || email.length < 4) {
+        seterror("mail", "*Invalid email length");
+        returnval = false;
+    }
+  
+    var subject = document.forms['myForm']["fsubject"].value;
+    if (subject.length === 0) {
+        seterror("subject", "*Subject is required");
+        returnval = false;
+    }
+  
+    var comment = document.forms['myForm']["fcomment"].value;
+    if (comment.length === 0) {
+        seterror("comment", "*Comment is required");
+        returnval = false;
+    }
+  
+    if (returnval) {
+        alert('Your message has been received');
+        document.forms['myForm'].reset();
+    }
+   
+  }
  
 
 
